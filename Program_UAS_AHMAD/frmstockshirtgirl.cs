56648927f2304e312b3b2f3ed0ad8ac5cbc85e2b
ahmad jeffry aryanto <ahmad.aryanto00@gmail.com>
2020-01-15 19:08:50 +0700

    Commit search query

--- Program_UAS_AHMAD/frmstockshirtgirl.cs
+++ Program_UAS_AHMAD/frmstockshirtgirl.cs
@@ -34,6 +34,19 @@ namespace Program_UAS_AHMAD
             DG.DataSource = ds.Tables["TB_PRODUCTS"].DefaultView;
         }
 
+        private void search()
+        {
+            ds.Clear();
+            string sql = "select ID,Nama_Item,harga,stok,kategori from TB_PRODUCTS where id='" + lblid.Text + txtid.Text + "' AND kategori='" + txtkat.Text + "'";
+            OleDbConnection con = new OleDbConnection(koneksi);
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(sql, con);
+
+            da.Fill(ds, "TB_PRODUCTS");
+            con.Close();
+            DG.DataSource = ds.Tables["TB_PRODUCTS"].DefaultView;
+        }
+
         private void frmstockshirtgirl_Load(object sender, EventArgs e)
         {
             tampil();
@@ -51,7 +64,7 @@ namespace Program_UAS_AHMAD
 
         private void btsearch_Click(object sender, EventArgs e)
         {
-
+            search();
         }
 
         private void btnew_Click(object sender, EventArgs e)
